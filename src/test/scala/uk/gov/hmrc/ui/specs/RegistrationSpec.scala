7b95d345d03a1195a43f9ab12654a694c1d28ecb
Susan Swinburne <susan.badel@opencastsoftware.com>
2025-06-09 13:01:54 +0100

    VEI-249 remove manual navigation to contact details

--- src/test/scala/uk/gov/hmrc/ui/specs/RegistrationSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/RegistrationSpec.scala
@@ -57,11 +57,7 @@ class RegistrationSpec extends BaseSpec {
       registration.checkJourneyUrl("add-website-address")
       registration.answerRadioButton("no")
 
-      //      The rest of the journey is not developed yet
-//      manually navigate to the Url
-
       Then("the intermediary enters credentials on contact-details page")
-      registration.goToPage("business-contact-details")
       registration.checkJourneyUrl("business-contact-details")
       registration.fillContactDetails("Testname", "12345678", "exampletest@mail.com")
 
