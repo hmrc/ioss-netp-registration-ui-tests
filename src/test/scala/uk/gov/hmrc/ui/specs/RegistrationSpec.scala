a1844e5f41bad2fd733c03b84311ef64ca6b24ca
Susan Swinburne <susan.badel@opencastsoftware.com>
2025-06-16 10:04:36 +0100

    VEI-233 added trading name pages to tests

--- src/test/scala/uk/gov/hmrc/ui/specs/RegistrationSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/RegistrationSpec.scala
@@ -25,7 +25,7 @@ class RegistrationSpec extends BaseSpec {
 
   Feature("Registration journeys") {
 
-    Scenario("Intermediary registers on behalf of a NETP using the IOSS NETP Registration service") {
+    Scenario("Intermediary registers on behalf of a NETP using the IOSS NETP Registration service - full answers") {
 
       Given("the intermediary accesses the IOSS NETP Registration Service")
       auth.goToAuthorityWizard()
@@ -36,6 +36,28 @@ class RegistrationSpec extends BaseSpec {
       registration.answerVatDetailsUkVrn()
       registration.checkJourneyUrl("confirm-vat-details")
       registration.answerVatDetails("yes")
+
+      When("the intermediary selects yes on the have-uk-trading-name page")
+      // Manual navigation until fix goes in for confirm-vat-details page
+      registration.goToPage("have-uk-trading-name")
+      registration.checkJourneyUrl("have-uk-trading-name")
+      registration.answerRadioButton("yes")
+
+      Then("the intermediary adds the first trading name")
+      registration.checkJourneyUrl("uk-trading-name/1")
+      registration.enterAnswer("1st trading-name")
+
+      And("the intermediary selects yes on the add-uk-trading-name page")
+      registration.checkJourneyUrl("add-uk-trading-name")
+      registration.answerRadioButton("yes")
+
+      And("the intermediary adds the second trading name")
+      registration.checkJourneyUrl("uk-trading-name/2")
+      registration.enterAnswer("Another Trading Name!")
+
+      And("the intermediary selects no on the add-uk-trading-name page")
+      registration.checkJourneyUrl("add-uk-trading-name")
+      registration.answerRadioButton("no")
 
 //      The rest of the journey is not developed yet
 
@@ -70,5 +92,51 @@ class RegistrationSpec extends BaseSpec {
 
       // Registration Pending to be added here when developed
     }
+
+    Scenario(
+      "Intermediary registers on behalf of a NETP using the IOSS NETP Registration service - only mandatory answers"
+    ) {
+
+      Given("the intermediary accesses the IOSS NETP Registration Service")
+      auth.goToAuthorityWizard()
+      auth.loginUsingAuthorityWizard(true, true, "standard")
+      registration.checkJourneyUrl("client-uk-based")
+
+      And("the intermediary answers all of the vat details questions as a UK Based NETP with VRN")
+      registration.answerVatDetailsUkVrn()
+      registration.checkJourneyUrl("confirm-vat-details")
+      registration.answerVatDetails("yes")
+
+      When("the intermediary selects no on the have-uk-trading-name page")
+      // Manual navigation until fix goes in for confirm-vat-details page
+      registration.goToPage("have-uk-trading-name")
+      registration.checkJourneyUrl("have-uk-trading-name")
+      registration.answerRadioButton("no")
+
+      //      The rest of the journey is not developed yet
+
+      Then("the intermediary adds the first client website address")
+      registration.goToPage("website-address/1")
+      registration.checkJourneyUrl("website-address/1")
+      registration.enterAnswer("www.1st-website.co.uk")
+
+      Then("the intermediary answers yes to add another client website address")
+      registration.checkJourneyUrl("add-website-address")
+      registration.answerRadioButton("no")
+
+      Then("the intermediary enters credentials on contact-details page")
+      registration.checkJourneyUrl("business-contact-details")
+      registration.fillContactDetails("Firstname Surname", "+44123456789", "test-email@test.co.uk")
+
+      //  CYA to be added here when developed
+
+      When("the intermediary accepts the declaration")
+      registration.goToPage("declaration")
+      registration.checkJourneyUrl("declaration")
+      registration.selectCheckbox()
+
+      // Registration Pending to be added here when developed
+    }
+
   }
 }
