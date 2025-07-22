ae4b01d4fea33f1a5507e5a14cde66613017599a
Susan Swinburne <susan.badel@opencastsoftware.com>
2025-07-22 14:08:07 +0100

    VEI-322 swap version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "ui-test-runner" % "0.47.0" % Test,
+    "uk.gov.hmrc" %% "ui-test-runner" % "0.46.0" % Test,
     "junit"        % "junit"          % "4.13.2" % Test
   )
 
