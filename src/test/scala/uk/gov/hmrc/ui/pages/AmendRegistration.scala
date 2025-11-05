4da004e7e83f9eda604cbf14d54f302c3a86bfa4
susan-swinburne <57998768+susan-swinburne@users.noreply.github.com>
2025-11-05 12:35:11 +0000

    VEI-579 initial updates for registration details section in amend reg… (#34)
    * VEI-579 initial updates for registration details section in amend registration
    
    * VEI-579 further scenario updates
    
    * VEI-579 finished off amend tests

--- src/test/scala/uk/gov/hmrc/ui/pages/AmendRegistration.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/AmendRegistration.scala
@@ -27,8 +27,7 @@ object AmendRegistration extends BasePage {
 
     clientType match {
 
-      // Note current change links added to checks but need adjusting accordingly in VEI-450/VEI-579
-      case "ukBasedUkVrn"    =>
+      case "ukBasedUkVrn"           =>
         Assert.assertTrue(
           body.contains(
             "Registration details\n" +
@@ -40,7 +39,7 @@ object AmendRegistration extends BasePage {
               "AA11 1AA"
           )
         )
-      case "ukBasedUtr"      =>
+      case "ukBasedUtr"             =>
         Assert.assertTrue(
           body.contains(
             "Registration details\n" +
@@ -59,7 +58,25 @@ object AmendRegistration extends BasePage {
               "NE11HM Change\n"
           )
         )
-      case "ukBasedNino"     =>
+      case "ukBasedUtrAmended"      =>
+        Assert.assertTrue(
+          body.contains(
+            "Registration details\n" +
+              "Based in UK Yes\n" +
+              "Has UK VAT registration number No\n" +
+              "Trading name Updated 3rd client name Change\n" +
+              //          Hidden change text start
+              "your Clients trading name\n" +
+              //          Hidden change text end
+              "Has Unique Taxpayer Reference (UTR) number Yes\n" +
+              "UTR number 1234567890\n" +
+              "Principal place of business address 1 New Address Line\n" +
+              "Other Address Line 2\n" +
+              "New Town-Name\n" +
+              "Other Region or State Change\n"
+          )
+        )
+      case "ukBasedNino"            =>
         Assert.assertTrue(
           body.contains(
             "Registration details\n" +
@@ -70,10 +87,7 @@ object AmendRegistration extends BasePage {
               "your Clients trading name\n" +
 //          Hidden change text end
               "Has Unique Taxpayer Reference (UTR) number No\n" +
-              "National Insurance number (NINO) 1234567890 Change\n" +
-//          Hidden change text start
-              "National Insurance number (NINO)\n" +
-//          Hidden change text end
+              "National Insurance number (NINO) AA112211A\n" +
               "Principal place of business address Other Address Line 1\n" +
               "Other Address Line 2\n" +
               "Other Town or City\n" +
@@ -81,7 +95,25 @@ object AmendRegistration extends BasePage {
               "NE11HM Change"
           )
         )
-      case "nonUkBasedUkVrn" =>
+      case "ukBasedNinoAmended"     =>
+        Assert.assertTrue(
+          body.contains(
+            "Registration details\n" +
+              "Based in UK Yes\n" +
+              "Has UK VAT registration number No" +
+              "\nTrading name Updated 8th client name Change\n" +
+              //          Hidden change text start
+              "your Clients trading name\n" +
+              //          Hidden change text end
+              "Has Unique Taxpayer Reference (UTR) number No\n" +
+              "National Insurance number (NINO) AA112211A\n" +
+              "Principal place of business address Other Address Line 1\n" +
+              "New Suburb\n" +
+              "Other Town or City\n" +
+              "NE11HM Change"
+          )
+        )
+      case "nonUkBasedUkVrn"        =>
         Assert.assertTrue(
           body.contains(
             "Registration details\n" +
@@ -92,40 +124,85 @@ object AmendRegistration extends BasePage {
 //          Hidden change text start
               "Country based in\n" +
 //          Hidden change text end
-              "Trading name Fifth Client Change\n" +
+              "Trading name in Spain Fifth Client Change\n" +
 //          Hidden change text start
               "your Clients trading name\n" +
 //          Hidden change text end
-              "Principal place of business address 1 The Street\n" +
-              "Some Town\n" +
-              "AA11 1AA"
+              "Principal place of business address 123 Street Name\n" +
+              "Suburb\n" +
+              "Barcelona\n" +
+              "ES123456 Change"
           )
         )
-      case "nonUkBasedFtr"   =>
+      case "nonUkBasedUkVrnAmended" =>
+        Assert.assertTrue(
+          body.contains(
+            "Registration details\n" +
+              "Based in UK No\n" +
+              "Has UK VAT registration number Yes\n" +
+              "UK VAT registration number 100000002\n" +
+              "Country based in Christmas Island Change\n" +
+              //          Hidden change text start
+              "Country based in\n" +
+              //          Hidden change text end
+              "Trading name in Christmas Island New 5th client name Change\n" +
+              //          Hidden change text start
+              "your Clients trading name\n" +
+              //          Hidden change text end
+              "Principal place of business address 123 Street Name\n" +
+              "New Suburb\n" +
+              "Barcelona\n" +
+              "State Change"
+          )
+        )
+      case "nonUkBasedFtr"          =>
         Assert.assertTrue(
           body.contains(
             "Based in UK No\n" +
               "Has UK VAT registration number No\n" +
-              "Country based in Spain Change\n" +
+              "Country based in France Change\n" +
 //          Hidden change text start
               "Country based in\n" +
 //          Hidden change text end
-              "National tax number 100000001 Change\n" +
+              "National tax number FR112233 Change\n" +
 //          Hidden change text start
               "your client's tax reference number\n" +
 //          Hidden change text end
-              "Trading name in Spain Seventh Client Change\n" +
+              "Trading name in France Seventh Client Change\n" +
 //          Hidden change text start
               "your Clients trading name\n" +
 //          Hidden change text end
-              "Principal place of business address Other Address Line 1\n" +
-              "Other Address Line 2\n" +
-              "Other Town or City\n" +
-              "Other Region or State\n" +
-              "NE11HM"
+              "Principal place of business address 100 Road Name\n" +
+              "Suburb\n" +
+              "Paris\n" +
+              "Region\n" +
+              "10050 Change"
           )
         )
-      case _                 => throw new Exception("This client type does not exist")
+      case "nonUkBasedFtrAmended"   =>
+        Assert.assertTrue(
+          body.contains(
+            "Based in UK No\n" +
+              "Has UK VAT registration number No\n" +
+              "Country based in Canada Change\n" +
+              //          Hidden change text start
+              "Country based in\n" +
+              //          Hidden change text end
+              "National tax number CA112233 Change\n" +
+              //          Hidden change text start
+              "your client's tax reference number\n" +
+              //          Hidden change text end
+              "Trading name in Canada New 7th client name Change\n" +
+              //          Hidden change text start
+              "your Clients trading name\n" +
+              //          Hidden change text end
+              "Principal place of business address 200 Street Name\n" +
+              "Suburb\n" +
+              "Paris\n" +
+              "Region Change"
+          )
+        )
+      case _                        => throw new Exception("This client type does not exist")
 
     }
   }
